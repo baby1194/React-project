c98e16a3260a494b7283f9355759c5f6353aa4b8
baby1194 <baby1194122537@gmail.com>
2024-03-02 23:52:28 +0100

    on vite.configs.js change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ReactProject/",
+  base: "/React-Project/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
