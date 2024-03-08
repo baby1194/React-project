e8a2300a0712225ddcb42d787e3ac88a83f2f96b
baby1194 <baby1194122537@gmail.com>
2024-03-08 19:52:54 +0100

    adding link to accordion from home (basic link - i well update it)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="flex flex-col gap-3 justify-start items-center">
       <p className="text-xl">this page under <span className="font-bold text-primary">development</span></p> 
       <p className="text-neutral-500 text-xs">I&apos;ll start on it when add the first componenet (project)</p>
+      <Link to='accordion'>accordion</Link>
     </div>
   );
 }
