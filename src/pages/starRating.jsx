0272147f3520bd74855439e0b4a354a855ada3f3
baby1194 <baby1194122537@gmail.com>
2024-03-19 12:22:48 +0000

    input to change number of start on real time and add shadcn input commponent

--- src/pages/starRating.jsx
+++ src/pages/starRating.jsx
@@ -1,39 +1,59 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa"; // Correct import for FaStar
+import { Input } from "@/components/ui/input";
 
 function StarRating({ numOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [starNums, setStarNums] = useState(numOfStars);
 
   function handleClick(getCurrentLIndex) {
-    setRating(getCurrentLIndex)
+    setRating(getCurrentLIndex);
   }
   function handleMouseEnter(getCurrentLIndex) {
-    setHover(getCurrentLIndex)
+    setHover(getCurrentLIndex);
   }
   function handleMouseLeave() {
-    setHover(rating)
+    setHover(rating);
   }
+  const handleStarNumChange = (event) => {
+    setStarNums(event.target.value);
+    console.log(starNums)
+  };
 
   return (
     <div
-      className={`container flex flex-1 items-center justify-center gap-8 my-10 w-full h-screen`}
+      className={`container flex flex-col flex-1 items-center justify-center gap-8 my-10 w-full h-screen`}
     >
-      {[...Array(numOfStars)].map((_, index) => {
-        index += 1;
+      <Input
+        type="number"
+        placeholder="number of stars"
+        className="w-72"
+        value={starNums}
+        onChange={handleStarNumChange}
+      />
 
-        return (
-          <FaStar
-            key={index}
-            className={index <= (hover || rating) ? 'active text-primary' : 'inactive text-white'}
-            onClick={() => handleClick(index)}
-            onMouseMove={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
-            size={40}
-          />
-        );
-      })}
+      <div className="flex justify-center items-center w-full gap-3">
+        {Array.from({length: starNums}).map((_, index) => {
+          index += 1;
+
+          return (
+            <FaStar
+              key={index}
+              className={
+                index <= (hover || rating)
+                  ? "active text-primary"
+                  : "inactive text-white"
+              }
+              onClick={() => handleClick(index)}
+              onMouseMove={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+              size={40}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
@@ -42,4 +62,4 @@ StarRating.propTypes = {
   numOfStars: PropTypes.number,
 };
 
-export default StarRating
+export default StarRating;
