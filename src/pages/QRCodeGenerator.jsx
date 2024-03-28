14c6af41e25d04043c0101f4fc7b845b967e99c7
baby1194 <baby1194122537@gmail.com>
2024-03-28 16:23:22 +0000

    finish QR code Generator

--- src/pages/QRCodeGenerator.jsx
+++ src/pages/QRCodeGenerator.jsx
@@ -1,8 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import QRCode from "react-qr-code";
+import { useState } from "react";
 
 const QRCodeGenerator = () => {
-  return (
-    <div>QRCodeGenerator</div>
-  )
-}
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
 
-export default QRCodeGenerator
+  function handleGenerateQrCode() {
+    setQrCode(input);
+    setInput("");
+  }
+
+  return (
+    <div
+      className={`container flex flex-col flex-1 items-center justify-center gap-8 my-10 w-full max-h-screen relative overflow-x-hidden`}
+    >
+      <h1>QR Code Generator</h1>
+      <div className="flex gap-3">
+        <Input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          name="qr-code"
+          value={input}
+          placeholder="Enter your value here"
+        />
+        <Button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={handleGenerateQrCode}
+          variant="secondary"
+        >
+          Generate
+        </Button>
+      </div>
+      <div>
+        <QRCode
+          id="qr-code-value"
+          value={qrCode}
+          size={400}
+          bgColor="transparent"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QRCodeGenerator;
