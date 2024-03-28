5deabc51a34bc22159f803b29a293f267fe00fe8
baby1194 <baby1194122537@gmail.com>
2024-03-28 10:30:33 +0000

    update tree view

--- src/pages/tree-view/TreeView.jsx
+++ src/pages/tree-view/TreeView.jsx
@@ -2,7 +2,7 @@ import MenuList from "./MenuList"
 
 function TreeView({menus = []})  {
   return (
-    <div className="container bg-blue-500 w-[260px] flex flex-1 py-5 mr-[100%]">
+    <div className="container bg-secondary w-[260px] flex flex-1 py-5 mr-[100%]">
       <MenuList list={menus} />
     </div>
   )
